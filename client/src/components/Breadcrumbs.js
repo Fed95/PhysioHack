47ee388d1397bbc16150a287bbb96245b37293de
Fed <fed@192.168.1.95>
2020-12-27 02:05:09 +0100

    better dummy

--- client/src/components/Breadcrumbs.js
+++ client/src/components/Breadcrumbs.js
@@ -3,13 +3,15 @@ import { HomeOutlined, UserOutlined } from '@ant-design/icons';
 
 export function Breadcrumbs() {
     return <Breadcrumb>
-        <Breadcrumb.Item href="">
+        <Breadcrumb.Item href="home">
             <HomeOutlined />
         </Breadcrumb.Item>
-        <Breadcrumb.Item href="">
+        <Breadcrumb.Item href="results">
             <UserOutlined />
-            <span>Application List</span>
+            <span>Search Results</span>
         </Breadcrumb.Item>
-        <Breadcrumb.Item>Application</Breadcrumb.Item>
+        <Breadcrumb.Item>
+            <span>The Fixer</span>
+        </Breadcrumb.Item>
     </Breadcrumb>
 }
