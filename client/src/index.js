e44ef6952818c3764ab467a72e9845263012c759
michelepapale1996 <michele.papale@mail.polimi.it>
2020-12-27 10:15:17 +0100

    Fixed conflicts

--- client/src/index.js
+++ client/src/index.js
@@ -2,91 +2,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
 import store from './redux/store';
 import 'antd/dist/antd.css';
 import {Layout, Row, Col, Input, Button} from 'antd';
-import { UserOutlined, HomeOutlined, SearchOutlined} from '@ant-design/icons';
+import {UserOutlined, HomeOutlined, SearchOutlined} from '@ant-design/icons';
 
 import './index.css';
 import "./style/profile.css"
 import {SearchBar} from "./components/Search"
 
-const { Header, Footer, Content } = Layout;
+const {Header, Footer, Content} = Layout;
 
 ReactDOM.render(
-  <React.StrictMode>
-<<<<<<< HEAD
-      <Layout>
-          <Header>
-              <Row style={{
-          textAlign: 'center',
-          alignItems: 'center'}}>
-                  <Col span={6}>PhysioHack!</Col>
-                  <Col span={6} className={"center"}>
-                      <SearchBar 
-                      placeholder="Cerca?"
-                      />
-                  </Col>
-                  <Col span={6} className={"center"}>
-                      <SearchBar 
-                      placeholder="Cerca?"
-                      />
-                  </Col>
-                  <Col span={5} className={"center"}>
-                      <Button type="primary" style={{ background: "#544E61", 
-                              borderColor: "#544E61" }} 
-                              icon={<SearchOutlined/>}>
-                            Cerca!
-                      </Button>
-                  </Col>
-                  <Col span={2} />
-              </Row>
-          </Header>
-          <Content className={"content"}>
-              <Row className={"contentHeader"}>
+    <React.StrictMode>
+        <Provider store={store}>
+            <Layout>
+                <Header>
+                    <Row style={{textAlign: 'center', alignItems: 'center'}}>
+                        <Col span={6}>PhysioHack!</Col>
+                        <Col span={6} className={"center"}>
+                            <SearchBar
+                                placeholder="Cerca?"
+                            />
+                        </Col>
+                        <Col span={6} className={"center"}>
+                            <SearchBar
+                                placeholder="Cerca?"
+                            />
+                        </Col>
+                        <Col span={5} className={"center"}>
+                            <Button type="primary" style={{
+                                background: "#544E61",
+                                borderColor: "#544E61"
+                            }}
+                                    icon={<SearchOutlined/>}>
+                                Cerca!
+                            </Button>
+                        </Col>
+                        <Col span={2}/>
+                    </Row>
+                </Header>
+                <Content className={"content"}>
+                    <Row className={"contentHeader"}>
 
-              </Row>
-              <Row style={{
-          textAlign: 'center',
-          alignItems: 'center'}}>
-                  <Col span={1} />
-                  <Col span={22}>
-                      <App />
-                  </Col>
-                  <Col span={1} />
-              </Row>
-          </Content>
-          <Footer>Footer</Footer>
-      </Layout>
-=======
-    <Provider store={store}>
-        <Header>
-            <Row>
-                <Col span={6}>Header</Col>
-                <Col span={16} className={"center"}>
-                    <SearchBar />
-                </Col>
-                <Col span={2} />
-            </Row>
-        </Header>
-        <Content className={"content"}>
-            <Row className={"contentHeader"}>
-
-            </Row>
-            <Row>
-                <Col span={1} />
-                <Col span={22}>
-                    <App />
-                </Col>
-                <Col span={1} />
-            </Row>
-        </Content>
-        <Footer>Footer</Footer>
-    </Provider>
->>>>>>> 3ce5dfd5ba53030054a796e96bd830bde2f0e362
-  </React.StrictMode>,
-  document.getElementById('root')
+                    </Row>
+                    <Row style={{textAlign: 'center', alignItems: 'center'}}>
+                        <Col span={1}/>
+                        <Col span={22}>
+                            <App/>
+                        </Col>
+                        <Col span={1}/>
+                    </Row>
+                </Content>
+                <Footer>Footer</Footer>
+            </Layout>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,10 @@
-<<<<<<< HEAD
-import React from 'react';
+import React, {useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { unwrapResult } from '@reduxjs/toolkit'
+import { useHistory } from "react-router-dom";
+
+import { fetchUsers } from '../redux/formSlice'
+
 import { Row, Col } from 'antd';
 import { Image } from 'antd';
 import 'antd/dist/antd.css';
@@ -9,63 +14,6 @@ import { UserOutlined, HomeOutlined, SearchOutlined} from '@ant-design/icons';
 import { Typography, Space } from 'antd';
 const { Text, Link, Title } = Typography;
 const { Header, Footer, Sider, Content } = Layout;
-
-function Home() {
-  return (
-    <div className="App">
-      	<Row style={{
-          textAlign: 'center',
-          alignItems: 'center'}}>
-    	<Col span={24}>
-	    <Image
-	      width={800}
-	      src="https://eliteorthocare.com/images/wp10523cbf_06.png"
-	    />
-	    </Col>
-	 </Row>
-    <Divider plain> . . . </Divider>
-	 <Row style={{backgroundColor: "lightblue", 
-	 			  padding: 10,
-				  textAlign: 'center',
-				  alignItems: 'center'}}>
-	 	<Col span={24}>
-      	<HomeForm></HomeForm>
-      	</Col>
-  	</Row>
-  	<Divider plain> . . . </Divider>
-  	<Row>
-    	<Col span={10} className={"center"}>
-	    <Image
-        width={150}
-        src={"https://image.flaticon.com/icons/png/512/387/387561.png"}
-      	/>
-      	</Col>
-      	<Col span={14} className={"center"}>
-      	<Title level={2}>Our Professionists</Title>
-      	</Col>
-	 </Row>
-	 <Row style={{
-          textAlign: 'center',
-          alignItems: 'center'}}>
-        <Col span={10} className={"center"}>
-      		<Title level={2}>Our Team</Title>
-      	</Col>
-      	<Col span={14} className={"center"}>
-      	<Image
-        width={150}
-        src={"https://images-na.ssl-images-amazon.com/images/I/41lCmegiT5L.png"}
-      	/>
-      	</Col>
-	 </Row>
-    </div>
-  );
-=======
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { unwrapResult } from '@reduxjs/toolkit'
-import { useHistory } from "react-router-dom";
-
-import { fetchUsers } from '../redux/formSlice'
 
 const Home = () => {
     const [address, setAddress] = useState('')
@@ -93,25 +41,70 @@ const Home = () => {
     }
 
     return (
-        <section>
-            <h2>Search</h2>
-            <form>
-                <label htmlFor="postTitle">Address:</label>
-                <input
-                    type="text"
-                    id="postTitle"
-                    name="postTitle"
-                    placeholder="Insert your address"
-                    value={address}
-                    onChange={onAddressChanged}
-                />
-                <button type="button" onClick={onSearchClicked} disabled={!canSave}>
-                    Search
-                </button>
-            </form>
-        </section>
-    )
->>>>>>> 3ce5dfd5ba53030054a796e96bd830bde2f0e362
+        <div className="App">
+            <Row style={{
+                textAlign: 'center',
+                alignItems: 'center'}}>
+                <Col span={24}>
+                    <Image
+                        width={800}
+                        src="https://eliteorthocare.com/images/wp10523cbf_06.png"
+                    />
+                </Col>
+            </Row>
+            <Divider plain> . . . </Divider>
+            <Row style={{backgroundColor: "lightblue",
+                padding: 10,
+                textAlign: 'center',
+                alignItems: 'center'}}>
+                <Col span={24}>
+                    <HomeForm></HomeForm>
+                </Col>
+            </Row>
+            <Divider plain> . . . </Divider>
+            <Row>
+                <Col span={10} className={"center"}>
+                    <Image
+                        width={150}
+                        src={"https://image.flaticon.com/icons/png/512/387/387561.png"}
+                    />
+                </Col>
+                <Col span={14} className={"center"}>
+                    <Title level={2}>Our Professionists</Title>
+                </Col>
+            </Row>
+            <section>
+                <h2>Search</h2>
+                <form>
+                    <label htmlFor="postTitle">Address:</label>
+                    <input
+                        type="text"
+                        id="postTitle"
+                        name="postTitle"
+                        placeholder="Insert your address"
+                        value={address}
+                        onChange={onAddressChanged}
+                    />
+                    <button type="button" onClick={onSearchClicked} disabled={!canSave}>
+                        Search
+                    </button>
+                </form>
+            </section>
+            <Row style={{
+                textAlign: 'center',
+                alignItems: 'center'}}>
+                <Col span={10} className={"center"}>
+                    <Title level={2}>Our Team</Title>
+                </Col>
+                <Col span={14} className={"center"}>
+                    <Image
+                        width={150}
+                        src={"https://images-na.ssl-images-amazon.com/images/I/41lCmegiT5L.png"}
+                    />
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default Home;
