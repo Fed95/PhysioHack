1257305455363c8b7fc8216cc1a349857cb9af39
ruggero panzeri <r.panzeri12@campus.unimib.it>
2020-12-27 12:04:18 +0100

    user model

---
+++ server/models/user.js
@@ -0,0 +1,117 @@
+require('dotenv').config({ path: './config/.env' });
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+const tokenConstant = require('./../constants/tokens')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    mail: {
+        unique: true,
+        type: String,
+        required: true
+    },
+    professionalId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Professional'
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    }
+}, {timestamps: true});
+
+UserSchema.methods.genereteAccessToken = function () {
+    let payload = {
+        id: this._id,
+        name: this.name,
+        surname: this.surname,
+        email: this.mail
+    }
+    return jwt.sign(payload, process.env.ACCESS_TOKEN, {
+        expiresIn: tokenConstant.ACCESS_TOKEN_EXPIRY
+    })
+}
+
+UserSchema.methods.verifyAccessToken = function (access_token) {
+    let user = this
+    return new Promise((resolve, reject)=>{
+        jwt.verify(accessToken, process.env.ACCESS_TOKEN, (err, payload)=> {
+            if(err){
+                resolve({
+                    payload:null
+                })
+            }
+            let new_payload = {...payload}
+            resolve({
+                payload:new_payload
+            })
+        })
+    }) 
+}
+
+UserSchema.methods.generateRefreshToken = function () {
+    let payload = {
+        id: this._id,
+        name: this.name,
+        surname: this.surname,
+        email: this.mail
+    }
+
+
+    return jwt.sign(payload, process.env.REFRESH_TOKEN, {
+        expiresIn: tokenConstant.REFRESH_TOKEN_EXPIRY
+    })
+}
+
+UserSchema.methods.verifyRefreshToken = function (refreshToken) {
+    let user = this
+    return new Promise((resolve, reject)=>{
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, payload)=> {
+            if(err){
+                resolve({
+                    payload:null
+                })
+            }
+            let new_payload = {...payload}
+            resolve({
+                payload:new_payload
+            })
+        })
+    }) 
+}
+
+UserSchema.pre('save', function (next) {
+    if (!this.isModified('password')) {
+        return next()
+    }
+
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) {
+            return next(err)
+        }
+
+        bcrypt.hash(this.password, salt, function (err, hash) {
+            if (err) {
+                return next(err)
+            }
+            this.password = hash
+            next()
+        })
+    })
+
+})
+
+
+module.exports = mongoose.model('User', UserSchema);
